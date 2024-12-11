e3e8449019e014cdf45ef987d97c879866d23452
datmicw <dat.micf@gmail.com>
2024-12-11 23:40:08 +0700

    Fix close and Open UI

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,37 +1,57 @@
 using System;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject panelAbout;
+    public GameObject panelOption;
     public GameObject retryPanel;
 
     private void Start()
     {
-        // Ẩn retryPanel khi game bắt đầu
-        if (retryPanel)
-        {
-            retryPanel.SetActive(false);
-        }
+        if (retryPanel) retryPanel.SetActive(false);
+        
+        if (panelOption)panelOption.SetActive(false);
+        
+        if(panelAbout)panelAbout.SetActive(false);
     }
-
+    public void OnOpenAboutButtonClick()
+    {
+        panelAbout.SetActive(true);
+    }
+    public void OnCloseAboutButtonClick()
+    {
+        panelAbout.SetActive(false);
+        
+    }
+    public void OnOpenOptionButtonClick()
+    {
+        panelOption.SetActive(true);
+    }
+    public void OnCloseOptionButtonClick()
+    {
+        panelOption.SetActive(false);
+    }
     // Khi người chơi nhấn nút Play, tải scene Main
     public void OnPlayerButtonClick()
     {
         SceneManager.LoadScene("Main"); 
     }
-
     // Khi người chơi nhấn nút Exit, thoát ứng dụng
-    public void OnExitButton()
+    public void OnExitToMenuButton()
+    {
+        SceneManager.LoadScene("Menu"); 
+    }
+    public void ExitGame()
     {
 #if UNITY_EDITOR
-        // Nếu đang chạy trong Unity Editor, chỉ dừng Play
-        UnityEditor.EditorApplication.isPlaying = false;
+        EditorApplication.isPlaying = false;
 #else
-         Application.Quit(); 
+            Application.Quit();
 #endif
     }
-
     public void ShowRetryUI()
     {
         // hiển thị Retry UI nếu đang ở trong scene game
@@ -40,7 +60,6 @@ public class MainMenu : MonoBehaviour
             retryPanel.SetActive(true); // Hiển thị UI Retry khi game over
         }
     }
-
     // Hàm để chơi lại game khi nhấn nút Retry
     public void RetryGame()
     {
