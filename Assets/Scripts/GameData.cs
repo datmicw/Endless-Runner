fe1188aea879c06a6f718e825411aa05539dd9bf
datmicw <dat.micf@gmail.com>
2025-01-31 22:40:01 +0700

    fix scale

--- Assets/Scripts/GameData.cs
+++ Assets/Scripts/GameData.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 using System.IO;
 public class GameData : MonoBehaviour
 {
@@ -15,18 +15,18 @@ public class GameData : MonoBehaviour
         if (Instance == null)
         {
             Instance = this;
-            DontDestroyOnLoad(gameObject);
+            DontDestroyOnLoad(gameObject); // Không hủy game object này khi chuyển scene
         }
         else
         {
             Destroy(gameObject);
         }
-        filePath = Path.Combine(Application.persistentDataPath, "gameData-EndlessRunner.json");
+        filePath = Path.Combine(Application.persistentDataPath, "gameData-EndlessRunner.json"); // Lấy đường dẫn lưu trữ dữ liệu
     }
     public void SaveGameData()
     {
-        string json = JsonUtility.ToJson(this);
-        File.WriteAllText(filePath, json);
+        string json = JsonUtility.ToJson(this); // Chuyển dữ liệu thành chuỗi json
+        File.WriteAllText(filePath, json); // Lưu dữ liệu vào file
         Debug.Log("Game data saved to: " + filePath);
     }
     public void LoadGameData()
